eea0f5ef784f1207b7de40738d2b00600dc319b4
grace <chanheeya@gmail.com>
2022-07-08 00:28:38 -0500

    correct tail with rspec

--- 7gyu.rb
+++ 7gyu.rb
@@ -1,2 +1,8 @@
-class 7gyu 
+class SevenGyu 
+
+  def self.correct_tail(body, tail)
+    last_letter = body[-1]
+    return true if tail == last_letter
+    false
+  end
 end
--- spec/7gyu_spec.rb
+++ spec/7gyu_spec.rb
@@ -1,4 +1,25 @@
-require "7gyu"
+require "./7gyu.rb"
 
-describe 7gyu do
+RSpec.describe SevenGyu, "#solve" do 
+  it "should get correct_tail" do
+    answer1 = SevenGyu.correct_tail("Fox", "x")
+    answer2 = SevenGyu.correct_tail("Rhino", "o")
+    answer3 = SevenGyu.correct_tail("Meerkat", "t")
+    answer4 = SevenGyu.correct_tail("Emu", "t")
+    answer5 = SevenGyu.correct_tail("Badger", "s")
+    answer6 = SevenGyu.correct_tail("Giraffe", "d")
+
+    expect(answer1).to eq(true)
+    expect(answer2).to eq(true)
+    expect(answer3).to eq(true)
+    expect(answer4).to eq(false)
+    expect(answer5).to eq(false)
+    expect(answer6).to eq(false)
+    # SevenGyu.assert_equals(correct_tail("Fox", "x"), true)
+    # 7gyu.assert_equals(correct_tail("Rhino", "o"), true)
+    # 7gyu.assert_equals(correct_tail("Meerkat", "t"), true)
+    # 7gyu.assert_equals(correct_tail("Emu", "t"), false)
+    # 7gyu.assert_equals(correct_tail("Badger", "s"), false)
+    # 7gyu.assert_equals(correct_tail("Giraffe", "d"), false)
+  end
 end
