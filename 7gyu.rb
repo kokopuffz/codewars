e90a2916a8be81e32ff489bdffdf08c23defdf33
grace <chanheeya@gmail.com>
2022-07-10 01:31:00 -0500

    vaporcode with testing

--- 7gyu.rb
+++ 7gyu.rb
@@ -1,8 +1,20 @@
 class SevenGyu 
 
   def self.correct_tail(body, tail)
-    last_letter = body[-1]
-    return true if tail == last_letter
-    false
+    body[-1] == tail
+    # body.end_with? tail
+    # last_letter = body[-1]
+    # last_letter == tail ? true : false
+    # return true if tail == last_letter
+    # false
+  end
+
+  def self.multiply(a, b)
+    a * b
+  end
+
+  def self.vaporcode(string)
+    # capitalize, delete all space, split by char, rejoin by double space
+    string.upcase.delete(" ").split("").join("  ")
   end
 end
--- spec/7gyu_spec.rb
+++ spec/7gyu_spec.rb
@@ -1,6 +1,7 @@
 require "./7gyu.rb"
 
 RSpec.describe SevenGyu, "#solve" do 
+
   it "should get correct_tail" do
     answer1 = SevenGyu.correct_tail("Fox", "x")
     answer2 = SevenGyu.correct_tail("Rhino", "o")
@@ -21,5 +22,21 @@ RSpec.describe SevenGyu, "#solve" do
     # 7gyu.assert_equals(correct_tail("Emu", "t"), false)
     # 7gyu.assert_equals(correct_tail("Badger", "s"), false)
     # 7gyu.assert_equals(correct_tail("Giraffe", "d"), false)
+  end
+
+  it "should multiply" do
+    answer1 = SevenGyu.multiply(1, 1)
+    answer2 = SevenGyu.multiply(2, 1)
+
+    expect(answer1).to eq(1)
+    expect(answer2).to eq(2)
+  end
+
+  it "should double space between char, capitalize" do
+    answer1 = SevenGyu.vaporcode("Lets go to the movies")
+    answer2 = SevenGyu.vaporcode("Why isnt my code working?")
+
+    expect(answer1).to eq("L  E  T  S  G  O  T  O  T  H  E  M  O  V  I  E  S")
+    expect(answer2).to eq("W  H  Y  I  S  N  T  M  Y  C  O  D  E  W  O  R  K  I  N  G  ?")
   end
 end
